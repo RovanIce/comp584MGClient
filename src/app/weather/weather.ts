5f776f9b1c5da31d0071f6d3ea47c7dffdb27f52
Michael Green <michaelgreen232@gmail.com>
2025-09-19 17:28:37 -0700

    9/19/2025 class table changes

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -9,7 +9,11 @@ import { WeatherData } from '../weather-data';
   styleUrl: './weather.scss'
 })
 export class Weather {
+  forecasts: WeatherData[] = [];
+  
   constructor(http: HttpClient) {
-    http.get<WeatherData[]>('http://localhost:5032/weatherforecast').subscribe(result => {})
+    http.get<WeatherData[]>('http://localhost:5032/weatherforecast').subscribe(result => {
+      this.forecasts= result;
+    });
   }
 }
