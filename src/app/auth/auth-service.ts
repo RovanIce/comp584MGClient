f7c27e5bdbe6ac1b2d0e1ebba093d7b971fd1221
Michael Green <michaelgreen232@gmail.com>
2025-12-05 18:08:25 -0800

    12/5/2025

--- src/app/auth/auth-service.ts
+++ src/app/auth/auth-service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { LoginRequest } from './login-request';
+import { LoginResponse } from './login-response';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private token = 'auth_token';
+  constructor(private http: HttpClient){}
+  Login(Loginrequest:LoginRequest):Observable<LoginResponse>{
+    return this.http.post<LoginResponse>(environment.apiurl + "api/Admin",Loginrequest)
+    .pipe(tap(response=>{
+      if(response.sucess){
+        localStorage.setItem(this.token,response.token);
+      }
+    }));
+  }
+  Logout(){
+    localStorage.removeItem(this.token);
+  }
+  IsAuthenticated():boolean{
+    return localStorage.getItem(this.token)!=null;
+  }
 }
--- src/app/auth/login.ts
+++ src/app/auth/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { AuthService } from './auth-service';
+import { LoginRequest } from './login-request';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
-export class Login {
+export class Login implements OnInit {
+ constructor(private authService: AuthService){
+ }
+ form!: UntypedFormGroup;
+ ngOnInit(): void {
+   this.form = new UntypedFormGroup({
+    userName: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
+   });
+ }
+ onSubmit(): void{
+    let loginrequest = <LoginRequest>{
+      userName: this.form.controls['userName'].value,
+      password: this.form.controls['password'].value
+    }
+    this.authService.Login(loginrequest).subscribe({
+      next: result => {
+        console.log(result);
+      },
+      error: result =>{
+        console.log("Error" + result);
+      }
+    })
+    
+ }
 
 }
